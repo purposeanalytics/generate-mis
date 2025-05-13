4f63ccec09125e38f2ffcfc9ef1ac2d266cfe9bd
Daniel Liadsky <daniel.l@purposeanalytics.ca>
2025-05-13 18:51:09 -0400

    Correct statistical code column names

--- data-raw/update_all_functional_centres_and_statistical_codes.R
+++ data-raw/update_all_functional_centres_and_statistical_codes.R
@@ -1,3 +1,3 @@
-all_functional_centres_and_statistical_codes <- readr::read_csv(r"(C:\Temp\westnh\mis_powerbi\outputs\statistical_codes.csv)")
+all_functional_centres_and_statistical_codes <- readr::read_csv(paste0(Sys.getenv("USERPROFILE"), r"(\Workspace\functional-centre-statistical-account-generation\output_files\statistical_codes.csv)"))
 
 usethis::use_data(all_functional_centres_and_statistical_codes, overwrite = TRUE)
